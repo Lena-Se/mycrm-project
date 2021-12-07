ab59f6f68b045bf309b3a8518c93748ce5ec9152
Lena-Se <94070326+Lena-Se@users.noreply.github.com>
2021-12-07 23:50:50 +0200

    add models and common dirs
    add Client, Phone and Email models
    add dirs for templates, static, media
    add template base and index

--- crm/models.py
+++ crm/models.py
@@ -1,14 +1,47 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+
 class Phone(models.Model):
-    number = models.CharField(max_length=13)
-    
+    number = models.CharField(max_length=13, verbose_name='телефон')
+    client = models.ForeignKey('Client', on_delete=models.SET_NULL, null=True, verbose_name='компания')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.number
 
 
+class Email(models.Model):
+    email_address = models.EmailField(verbose_name='email')
+    client = models.ForeignKey('Client', on_delete=models.SET_NULL, null=True, verbose_name='компания')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.address
 
 
 class Client(models.Model):
-    name = models.CharField(max_length=500)
-    contact_person = models.CharField(max_length=300)
+    company_name = models.CharField(max_length=500, verbose_name='компания')
+    contact_person = models.CharField(max_length=300, verbose_name='контактное лицо (ФИО)')
+    description = models.TextField(blank=True, verbose_name='описание')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='создан')
+    updated = models.DateTimeField(auto_now=True, verbose_name='изменен')
+    address = models.CharField(max_length=500, blank=True, verbose_name='адрес')
+    slug = models.SlugField(max_length=250, unique=True)
+
+    class Meta:
+        ordering = ('company_name', 'created', 'updated')
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
+
+    def __str__(self):
+        return self.company_name
+
 
